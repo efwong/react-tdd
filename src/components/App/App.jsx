45dd6d5a67209aade327dbdcbb63b35d50183206
Edwin <edwinfwong@gmail.com>
2021-11-26 03:15:29 -0800

    initial tdd

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Calculator from './../Calculator/Calculator';
+
+const App = () => {
+  return (
+    <div className='app-container'>
+      <span>Test</span>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
